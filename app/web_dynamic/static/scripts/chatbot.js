0d4d15f9b663f677d825f3168e894f2f114397df
Paul Njuga <paulnjuga@gmail.com>
2023-11-08 21:01:16 +0300

    Implement add for generated tasks

--- app/web_dynamic/static/scripts/chatbot.js
+++ app/web_dynamic/static/scripts/chatbot.js
@@ -5,7 +5,7 @@ const sendChatBtn = document.querySelector('.chat-input span');
 let userMessage = null;
 
 const createChatLi = (message, className) => {
-  // Create a chat <li> element with passed message and className
+  /* Create a chat <li> element with passed message and className */
   const chatLi = document.createElement('li');
   chatLi.classList.add('chat', `${className}`);
   const chatContent =
@@ -14,19 +14,10 @@ const createChatLi = (message, className) => {
       : '<span class="material-symbols-outlined">smart_toy</span><p></p>'; /* Add task icon after <p> tags */
   chatLi.innerHTML = chatContent;
   chatLi.querySelector('p').textContent = message;
-  return chatLi; // return chat <li> element
+  return chatLi;
 };
 
-const displayTasks = (tasks, msgElement) => {
-  Object.keys(tasks).forEach(index => {
-    const task = tasks[index];
-    const title = task.title;
-    msgElement.textContent = title;
-  });
-};
-
-const getResponse = (chatElement) => {
-  const messageElement = chatElement.querySelector('p');
+const getResponse = () => {
   const requestOptions = {
     method: 'POST',
     headers: {
@@ -35,41 +26,95 @@ const getResponse = (chatElement) => {
     body: JSON.stringify({ user_input: userMessage })
   };
 
-  // Send POST request to chat_bot route & get json response
-  // set the reponse as paragraph text
-  const response = fetch('/chatbot', requestOptions)
-    .catch(() => {
-      messageElement.textContent =
-        'Oops! Something went wrong. Please try again.';
+  /* Display 'Thinking...' message while waiting for the response */
+  const pre = createChatLi('Thinking...', 'incoming');
+  chatbox.appendChild(pre);
+  const messageElement = pre.querySelector('p');
+
+  /*
+  Send POST request to chat_bot route & get json response
+  JSON format = {0 : {'title': 'Task title', 'summary': 'Task summary'}}
+  */
+  fetch('/chatbot', requestOptions).then(res => res.json()).then(data => {
+    const thinkingMessage = chatbox.querySelector('.incoming:last-of-type');
+    if (thinkingMessage) {
+      chatbox.removeChild(thinkingMessage);
+    }
+    Object.keys(data).forEach(index => {
+      /* Create a new <p> element for each task received */
+      const newTaskElement = createChatLi(data[index].title, 'incoming');
+      newTaskElement.setAttribute('data-name', data[index].summary);
+
+      /* Append a button to the paragraph */
+      const taskParagraph = newTaskElement.querySelector('p');
+      const button = document.createElement('button');
+      button.classList.add('add-task-btn');
+      const image = document.createElement('img');
+      image.src = '../static/svg/add.svg';
+      image.alt = '';
+      image.classList.add('add-task-img');
+      button.appendChild(image);
+      taskParagraph.insertAdjacentElement('afterend', button);
+
+      /* Handle click btn op */
+      button.addEventListener('click', () => {
+        const taskTitle = newTaskElement.querySelector('p').textContent;
+        const taskDescription = newTaskElement.getAttribute('data-name');
+
+        /* Send task details to the add-task route */
+        sendTaskToServer(taskTitle, taskDescription);
+      });
+
+      /* Append the <li> to the chatbox */
+      chatbox.appendChild(newTaskElement);
     });
-  const tasks = response.json();
-  displayTasks(tasks, messageElement);
-  chatbox.scrollTo(0, chatbox.scrollHeight);
+  }).catch(() => {
+    messageElement.textContent = 'Oops! Something went wrong. Please try again.';
+  }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
+
+const sendTaskToServer = (taskTitle, taskDescription) => {
+  fetch('/add_gen_task', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ title: taskTitle, description: taskDescription })
+  })
+    .then(response => {
+      if (response.ok) {
+        const genTskBtn = document.querySelector('.add-task-btn');
+        genTskBtn.image.src = '../static/svg/add-gen-task.svg';
+      }
+    })
+    .catch(error => {
+      console.error('Error adding task:', error);
+    });
 };
 
 const handleChat = () => {
-  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+  userMessage = chatInput.value.trim(); /* Get user entered message and remove extra whitespace */
   if (!userMessage) return;
 
-  // Clear the input textarea
+  /* Clear the input textarea */
   chatInput.value = '';
 
-  // Append the user's message to the chatbox
+  /* Append the user's message to the chatbox */
   chatbox.appendChild(createChatLi(userMessage, 'outgoing'));
   chatbox.scrollTo(0, chatbox.scrollHeight);
 
   setTimeout(() => {
-    // Display 'Thinking...' message while waiting for the response
-    const incomingChatLi = createChatLi('Thinking...', 'incoming');
-    chatbox.appendChild(incomingChatLi);
     chatbox.scrollTo(0, chatbox.scrollHeight);
-    getResponse(incomingChatLi);
+    getResponse();
   }, 600);
 };
 
 chatInput.addEventListener('keydown', (e) => {
-  // If Enter key is pressed without Shift key and the window
-  // width is greater than 800px, handle the chat
+  /*
+  If Enter key is pressed without Shift key,
+  and the window width is greater than 800px,
+  handle the chat
+  */
   if (e.key === 'Enter' && !e.shiftKey && window.innerWidth > 800) {
     e.preventDefault();
     handleChat();
